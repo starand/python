93b285bee843ed85da3a444868257ef73906477d
Andriy.Starodub <Andriy.Starodub@eleks.com>
2014-09-06 14:25:16 +0300

    .

--- test/client.py
+++ test/client.py
@@ -14,9 +14,11 @@ class TerminalsClient:
         try:
             self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.socket.connect((self.host, self.port))
-            print('Connectin to {0}:{1} established'.format(self.host, self.port))
+            print('Connection to {0}:{1} established'.format(self.host, self.port))
+            return True
         except :
             print('Could not connect to {0}:{1}'.format(self.host, self.port))
+            return False
 
     def close_connection(self):
         print('Conection to {0}:{1} was closed'.format(self.host, self.port))
@@ -24,11 +26,10 @@ class TerminalsClient:
             
     def send_commands(self):
         while self.running:
-            line = raw_input()
+            line = raw_input('\033[92m#\033[0m ')
             send_string(self.socket, line)
 
     def recv_responses(self):
-        self.init_connection()
         while self.running:
             try:
                 reply = receive_string(self.socket)
@@ -39,13 +40,27 @@ class TerminalsClient:
                 self.init_connection()
 
     def start(self):
-        self.recv_thread = Thread(target=self.recv_responses)
-        self.recv_thread.start()
-        self.send_thread = Thread(target=self.send_commands)
-        self.send_thread.start()
+        if self.init_connection():
+            self.recv_thread = Thread(target=self.recv_responses)
+            self.recv_thread.start()
+            self.send_thread = Thread(target=self.send_commands)
+            self.send_thread.start()
 
-client = TerminalsClient()
-client.start()
+#client = TerminalsClient()
+#client.start()
 
-while True:
-     pass
+
+def has_colours(stream):
+    if not hasattr(stream, "isatty"):
+        return False
+    if not stream.isatty():
+        return False # auto color only on TTYs
+    try:
+        import curses
+        curses.setupterm()
+        return curses.tigetnum("colors") > 2
+    except:
+        # guess false in case of error
+        return False
+        
+print has_colours(sys.stdout)
